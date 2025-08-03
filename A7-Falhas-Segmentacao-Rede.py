a8165e7f84c0965ba2ba2d78f670e902776cdf3e
MarianaSiano <marianasianop@gmail.com>
2025-08-02 21:01:32 -0300

    Teste de Conectividade a Nível de Rede concluido

--- A7-Falhas-Segmentacao-Rede.py
+++ A7-Falhas-Segmentacao-Rede.py
@@ -11,3 +11,17 @@ TARGET_NF_IP = "10.0.2.1" #Exemplo: IP da NF que deveria estar isolada na Slice 
 TARGET_NF_PORT = 29510 #Exemplo: porta do NRF na Slice B
 
 print("[+] Testando isolamento de Network Slicing...")
+
+#Teste 1: Teste de conectividade a nível de rede (socket)
+def test_tcp_port_conection(ip, port):
+    try:
+        with socket.create_connection((ip, port), timeout=3):
+            return True
+    except (socket.timeout, socket.error):
+        return False
+
+if test_tcp_port_conection(TARGET_NF_IP, TARGET_NF_PORT):
+    print(f"[!!!] VULNERABILIDADE DETECTADA: Falha na segmentação de rede.")
+    print(f"      Um host na Slice A pode se conectar via TCP a {TARGET_NF_IP}:{TARGET_NF_PORT} na Slice B.")
+else:
+    print(f"[+] Conexão TCP para {TARGET_NF_IP}:{TARGET_NF_PORT} falhou. A segmentação parece estar correta.")
