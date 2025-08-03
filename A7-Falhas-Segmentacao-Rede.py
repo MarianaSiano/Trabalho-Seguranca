ec726c4b40fde111e2de787aaa9759886e1a76c6
MarianaSiano <marianasianop@gmail.com>
2025-08-02 21:07:02 -0300

    Teste de bypass de autorização a nivel de API feita e o arquivo 'A7-Falhas-Segmentacao-Rede' concluido

--- A7-Falhas-Segmentacao-Rede.py
+++ A7-Falhas-Segmentacao-Rede.py
@@ -25,3 +25,20 @@ if test_tcp_port_conection(TARGET_NF_IP, TARGET_NF_PORT):
     print(f"      Um host na Slice A pode se conectar via TCP a {TARGET_NF_IP}:{TARGET_NF_PORT} na Slice B.")
 else:
     print(f"[+] Conexão TCP para {TARGET_NF_IP}:{TARGET_NF_PORT} falhou. A segmentação parece estar correta.")
+
+#Teste 2: Teste de bypass de autorização (a nível de API)
+TOKEN_DA_SLICE_A = "[TOKEN_DA_MINHA_SLICE]"
+HEADERS = {"Authorization": f"Bearer {TOKEN_DA_SLICE_A}"}
+UDM_DA_SLICE_B_URL = f"https://{TARGET_NF_IP}:29503/nudm-uecm/v1/ues/imsi-208930000000002/registrations"
+
+try:
+    response = requests.get(UDM_DA_SLICE_B_URL, headers=HEADERS, verify=False, timeout=5)
+    if response.status_code == 200:
+        print("[!!!] VULNERABILIDADE DETECTADA: Bypass de autorização inter-slice!")
+        print(f"      A NF na Slice A obteve acesso aos dados da Slice B. Resposta: {response.text[:100]}...")
+    elif response.status_code == 403 or response.status_code == 401:
+        print("[+] Bypass de autorização falhou (acesso negado).")
+    else:
+        print(f"[-] Resposta inesperada: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"[-] Erro ao tentar acessar a API da Slice B: {e}")
