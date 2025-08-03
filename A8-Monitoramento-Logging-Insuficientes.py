acaeaef5d9b9a6582719ad7bb0ea8549eecff575
MarianaSiano <marianasianop@gmail.com>
2025-08-02 21:13:46 -0300

    Código base inicial concluido

--- A8-Monitoramento-Logging-Insuficientes.py
+++ A8-Monitoramento-Logging-Insuficientes.py
@@ -3,3 +3,10 @@ import urllib3
 import time
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+#Configurações do teste
+NF_LOGIN_URL = "https://127.0.0.1:8000/auth/login"
+NF_PROTECTED_URL = "https://127.0.0.1:29510/api/secure_resource"
+NF_INVALID_URL = "https://127.0.0.1:29510/api/invalid_endpoit"
+
+print("[+] Simulando eventos de segurança para teste de logging...")
