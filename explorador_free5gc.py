e2761174e3d8670960fc897584937ce6f6a5cafa
MarianaSiano <marianasianop@gmail.com>
2025-08-07 22:00:47 -0300

    Funções atualizadas

--- explorador_free5gc.py
+++ explorador_free5gc.py
@@ -1,6 +1,7 @@
 import requests
 import socket
 import ssl
+import subprocess
 import urllib3
 import matplotlib.pyplot as plt
 
@@ -15,20 +16,22 @@ NF_CONFIG = {
     "SUPI_2": "208930000000002"
 }
 
+#A2 - Cryptographic Failures
 def test_cryptographic_failures():
     print("=" * 50)
-    print("A2 - Testando Criptografia")
+    print("A2 - Testando Cryptografic Failures")
     print("=" * 50)
 
     http_insecure, tls_weak = 0, 0
     if NF_CONFIG["NRF_SCHEME"] == "http":
-        print("[!!!] HTTP inseguro em uso")
+        print("[!!!] HTTP inseguro em uso.")
         http_insecure = 1
     return http_insecure, tls_weak
 
+#A9 - Logging
 def test_logging():
     print("=" * 50)
-    print("Testando Logs")
+    print("A9 - Testando Logs")
     print("=" * 50)
 
     login_fails, endpoint_invalid = 0, 0
@@ -40,9 +43,10 @@ def test_logging():
             requests.post(f"http://{NF_CONFIG['NRF_IP']}:{NF_CONFIG['NRF_PORT']}/api/invalid_endpoint", json={}, timeout=1)
             endpoint_invalid += 1
     except Exception as e:
-        print("Erro", e)
+        print("Erro: ", e)
     return login_fails, endpoint_invalid
 
+#Segmentação
 def get_access_token():
     try:
         r = requests.post(f"http://{NF_CONFIG['NRF_IP']}:{NF_CONFIG['NRF_PORT']}/nnrf-nfm/v1/oauth2/token", headers={"Content-Type": "application/x-www-form-urlencoded"}, data="grant_type=client_credentials&client_id=NSSF", timeout=5)
@@ -64,29 +68,9 @@ def test_network_slicing():
 
     token = get_access_token()
     if token:
-        r = requests.get(f"http://{NF_CONFIG['UDM_IP']}:{NF_CONFIG['UDM_PORT']}/nudm-uecm/v1/ues/imsi-{NF_CONFIG['SUPI_2']}/authentications", headers={"Authorization": f"Bearer {token}"}, timeout=5)
+        r = requests.get(
+            f"http://{NF_CONFIG['UDM_IP']}:{NF_CONFIG['UDM_PORT']}/nudm-uecm/v1/ues/imsi-{NF_CONFIG['SUPI_2']}/authentications",
+            headers={"Authorization": f"Bearer {token}"}, timeout=5)
         if r.status_code in [200, 201]:
             bypass_auth = 1
     return interslice_tcp, bypass_auth
-
-def gerar_grafico_linha(resultados):
-    categorias = [
-        "HTTP não seguro", "TLS fraco",
-        "Login inválido", "Endpoint inválido",
-        "Conexão inter-slice", "Bypass autorização"
-    ]
-
-    plt.plot(categorias, resultados, marker='o', linestyle='-', color='blue')
-    plt.title("Exploração de Vulnerabilidades no free5GC")
-    plt.ylabel("Ocorrências")
-    plt.xticks(rotation=30)
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig("grafico_vulnerabilidades.png")
-    plt.show()
-
-if __name__ == "__main__":
-    r1, r2 = test_cryptographic_failures()
-    r3, r4 = test_logging()
-    r5, r6 = test_network_slicing()
-    gerar_grafico_linha([r1, r2, r3, r4, r5, r6])
